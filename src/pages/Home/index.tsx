645fa58e8a24cb8559a4d8fb5361a35b875024ae
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 17:53:20 +0300

    feat: implement search form and enable API integration for name search in home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { MealCard } from '../../components/MealCard';
 import { usePartyContext } from '../../context';
@@ -6,42 +6,101 @@ import './home.scss';
 
 export const Home = () => {
   const navigate: any = useNavigate();
-  const [state]: any = usePartyContext();
+  const [state, { dispatch, getSearchData }]: any = usePartyContext();
   const latestData = state && state?.latestData;
   const randomData = state && state?.randomData;
+  const searchData = state && state?.searchData;
+
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [searchText, setSearchText] = useState<string>('');
+  const [searchList, setSearchList] = useState<any[]>(searchData);
+
+  const searchMeals = async () => {
+    if (!isLoading) {
+      setIsLoading(true);
+      await getSearchData(searchText);
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    if (searchText) {
+      setSearchList(searchData);
+    } else {
+      dispatch({
+        type: 'searchData',
+        payload: []
+      });
+      setSearchList([]);
+    }
+    // eslint-disable-next-line
+  }, [searchData, searchText]);
 
   return (
     <div className="home-container">
       <div>
-        <div>
-          <h4>Latest Meals</h4>
-          <div className="home-meal-group">
-            {latestData.map((item: any) => {
-              return (
-                <MealCard
-                  key={item.idMeal}
-                  image={item.strMealThumb}
-                  title={item.strMeal}
-                  onClick={() => navigate(`/meals/${item.idMeal}`)}
-                />
-              );
-            })}
-          </div>
+        <div className="search-controller">
+          <input
+            type="text"
+            value={searchText}
+            placeholder="Text string"
+            onChange={(e: any) => setSearchText(e.target.value)}
+          />
+          <button disabled={isLoading} onClick={searchMeals}>
+            Search
+          </button>
         </div>
-        <div>
-          <h4>Random Meals</h4>
-          <div className="home-meal-group">
-            {randomData.map((item: any) => {
-              return (
-                <MealCard
-                  key={item.idMeal}
-                  image={item.strMealThumb}
-                  title={item.strMeal}
-                  onClick={() => navigate(`/meals/${item.idMeal}`)}
-                />
-              );
-            })}
-          </div>
+        <div className="meals-group">
+          {searchText && searchList?.length > 0 ? (
+            <div>
+              <h4>"{searchText}" Result</h4>
+              <div className="home-meal-group">
+                {searchList.map((item: any) => {
+                  return (
+                    <MealCard
+                      key={item.idMeal}
+                      image={item.strMealThumb}
+                      title={item.strMeal}
+                      onClick={() => navigate(`/meals/${item.idMeal}`)}
+                    />
+                  );
+                })}
+              </div>
+            </div>
+          ) : (
+            <>
+              <div>
+                <h4>Latest Meals</h4>
+                <div className="home-meal-group">
+                  {latestData.map((item: any) => {
+                    return (
+                      <MealCard
+                        key={item.idMeal}
+                        image={item.strMealThumb}
+                        title={item.strMeal}
+                        onClick={() => navigate(`/meals/${item.idMeal}`)}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+              <div>
+                <h4>Random Meals</h4>
+                <div className="home-meal-group">
+                  {randomData.map((item: any) => {
+                    return (
+                      <MealCard
+                        key={item.idMeal}
+                        image={item.strMealThumb}
+                        title={item.strMeal}
+                        onClick={() => navigate(`/meals/${item.idMeal}`)}
+                      />
+                    );
+                  })}
+                </div>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </div>
