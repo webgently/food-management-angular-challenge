7ba941f18418de0cc645c855f338b9ae1d756077
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 14:05:51 +0300

    feat: add home, filter and category routers

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,5 +1,16 @@
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home } from '../pages/Home';
+import { Category } from '../pages/Category';
+import { Filter } from '../pages/Filter';
 
 export const Router = () => {
-  return <BrowserRouter></BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/filter" element={<Filter />} />
+        <Route path="/category" element={<Category />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
