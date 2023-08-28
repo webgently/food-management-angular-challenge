5d21fa976cc1740ce3bdc175fb3e7455bca8eb1c
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 14:28:17 +0300

    feat: integrate layout section within the router component

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Layout } from '../layout';
 import { Home } from '../pages/Home';
 import { Category } from '../pages/Category';
 import { Filter } from '../pages/Filter';
@@ -6,11 +7,13 @@ import { Filter } from '../pages/Filter';
 export const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/filter" element={<Filter />} />
-        <Route path="/category" element={<Category />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/filter" element={<Filter />} />
+          <Route path="/category" element={<Category />} />
+        </Routes>
+      </Layout>
     </BrowserRouter>
   );
 };
