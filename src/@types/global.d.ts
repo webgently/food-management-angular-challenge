600d599c9c02c68d20561109b409257554ade8ce
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 14:08:02 +0300

    feat: declare nav list type

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -4,7 +4,14 @@ declare module '.png';
 declare module 'url:*' {
   export default string;
 }
+
 declare module '*.svg' {
   const content: any;
   export default content;
+}
+
+declare interface NavListObject {
+  label: string;
+  route: string;
+  shadow: string;
 }
