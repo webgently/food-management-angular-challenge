8a12022f3cb7b2589f4c3ee396bdf5ab391b9f3e
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 16:27:23 +0300

    feat: add router for meal detail page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,6 +3,7 @@ import { Layout } from '../layout';
 import { Home } from '../pages/Home';
 import { Category } from '../pages/Category';
 import { Filter } from '../pages/Filter';
+import { Meal } from '../pages/Meal';
 
 export const Router = () => {
   return (
@@ -12,6 +13,7 @@ export const Router = () => {
           <Route path="/" element={<Home />} />
           <Route path="/filter" element={<Filter />} />
           <Route path="/category" element={<Category />} />
+          <Route path="/meals/:id" element={<Meal />} />
         </Routes>
       </Layout>
     </BrowserRouter>
