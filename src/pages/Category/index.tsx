d5905ef153fe8459662c559e3435e4a9162590a9
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 17:35:36 +0300

    feat: implement API integration and frontend for category page

--- src/pages/Category/index.tsx
+++ src/pages/Category/index.tsx
@@ -1,6 +1,20 @@
 import React from 'react';
+import { usePartyContext } from '../../context';
+import { MealCard } from '../../components/MealCard';
 import './category.scss';
 
 export const Category = () => {
-  return <></>;
+  const [state]: any = usePartyContext();
+  const mealCategories = state && state?.mealCategories;
+
+  return (
+    <div className="category-container">
+      <div>
+        {mealCategories &&
+          mealCategories.map((item: any) => {
+            return <MealCard key={item.strCategory} image={item.strCategoryThumb} title={item.strCategory} />;
+          })}
+      </div>
+    </div>
+  );
 };
