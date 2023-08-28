6934f6670d1f357463e90dc46d699033d19e9a09
webgently <oleksandrshkabura811@gmail.com>
2023-08-28 14:52:38 +0300

    feat: implement API integration and frontend for home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,49 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { MealCard } from '../../components/MealCard';
+import { usePartyContext } from '../../context';
 import './home.scss';
 
 export const Home = () => {
-  return <></>;
+  const navigate: any = useNavigate();
+  const [state]: any = usePartyContext();
+  const latestData = state && state?.latestData;
+  const randomData = state && state?.randomData;
+
+  return (
+    <div className="home-container">
+      <div>
+        <div>
+          <h4>Latest Meals</h4>
+          <div className="home-meal-group">
+            {latestData.map((item: any) => {
+              return (
+                <MealCard
+                  key={item.idMeal}
+                  image={item.strMealThumb}
+                  title={item.strMeal}
+                  onClick={() => navigate(`/meals/${item.idMeal}`)}
+                />
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          <h4>Random Meals</h4>
+          <div className="home-meal-group">
+            {randomData.map((item: any) => {
+              return (
+                <MealCard
+                  key={item.idMeal}
+                  image={item.strMealThumb}
+                  title={item.Title}
+                  onClick={() => navigate(`/meals/${item.idMeal}`)}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
